160024f197d8cd3205b8e6a9b904c0e92e1d49b8
TanviSaxena03 <155883356+TanviSaxena03@users.noreply.github.com>
2024-01-08 18:05:49 +0530

    modified

--- Task_1.py
+++ Task_1.py
@@ -3,6 +3,7 @@ done = []
 
 def insert(i):
     task.append(i)
+    print("Task has been Added successfully/n")
     
 def delete(i):
     print("Task '"+i+"' is deleted\n" )
@@ -32,26 +33,25 @@ ch = 0
 while (ch != 5):
     print("1) Add a new Task\n")
     print("2) Delete a Task\n")
-    print("3) Mark a task as Completed\n")
+    print("3) Mark a Task as Completed\n")
     print("4) Display the To-Do List\n")
     print("5) Exit\n")
-    ch = input ("\n Enter your choice: ")
+    
+    ch = input ("Enter your choice: ")
     if (ch == '1'):
         a = input("\nEnter Task: ")
         insert(a)
+        
     elif (ch == '2'):
         a = input("\nEnter Task: ")
         delete(a)
+        
     elif (ch == '3'):
         a = input("\nEnter Task: ")
         completed(a)
+        
     elif (ch == '4'):
         show()
+        
     else:
         exit()
-        
-    
-    
-        
-        
-
