4449e0c912a72b7fc7f46a032293a95cde7673a1
TanviSaxena03 <155883356+TanviSaxena03@users.noreply.github.com>
2024-01-08 18:09:27 +0530

    exit updation

---
+++ Task_2.py
@@ -0,0 +1,40 @@
+def add(a, b):
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    if (b != 0):
+        return (a / b)
+    return ("Cannot be divided by 0")
+
+ch = 0
+while (ch != 5):
+    print("1) Add two numbers\n")
+    print("2) Subtract two numbers \n")
+    print("3) Multiply two numbers\n")
+    print("4) Divide two numbers\n")
+    print("5) Exit\n")
+    
+    ch = input ("Enter your choice: ")
+    x = eval (input ("\nEnter 1st Number: "))
+    y = eval (input ("\nEnter 2nd Number: "))
+    
+    if (ch == '1'):
+        print(x," + ",y," = ",add(x,y))
+        
+    elif (ch == '2'):
+        print(x," - ",y," = ",sub(x,y))
+        
+    elif (ch == '3'):
+        print(x," * ",y," = ",mul(x,y))
+        
+    elif (ch == '4'):
+        print(x," / ",y," = ",div(x,y))
+        
+    else:
+        exit()
